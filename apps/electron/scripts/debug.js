83a410617092d0f6093845768c3af599be787e15
Adam Sherwood <adam.the.sherwood@gmail.com>
2023-09-21 23:03:53 +0100

    try to change permissions again

--- apps/electron/scripts/debug.js
+++ apps/electron/scripts/debug.js
@@ -18,12 +18,24 @@ for (let p of paths) {
   }
 }
 
-let ex =
+var exec = require("child_process").exec;
+function execute(command, callback, err_callback) {
+  exec(command, function (error, stdout, stderr) {
+    callback && callback(stdout);
+    err_callback && err_callback(error);
+    err_callback && err_callback(stderr);
+  });
+}
+
+let ex_path =
   "/Users/runner/work/electron_build_bug_minimal_repro/electron_build_bug_minimal_repro/node_modules/app-builder-bin/mac/app-builder_amd64";
 try {
-  console.log("\nMODE BEFORE:", fs.statSync(ex).mode);
-  fs.chmodSync(ex, 0o777);
-  console.log("MODE AFTER:", fs.statSync(ex).mode, "\n");
+  console.log("\nMODE BEFORE:", fs.statSync(ex_path).mode);
+  fs.chmodSync(ex_path, 0o777);
+  execute("chmod +x " + ex_path, null, (error) => {
+    console.error(error);
+  });
+  console.log("MODE AFTER:", fs.statSync(ex_path).mode, "\n");
 } catch (e) {
   console.error("\nFAILED TO CHMOD\n");
 }
